b652d04a13faf277a377e6cbdc48d03984bb4bc2
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-03-01 01:58:50 +0530

    Create Color.kt

---
+++ app/src/main/java/com/teristaspace/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.teristaspace.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val TeristaPrimary = Color(0xFF6366F1)
+val TeristaSecondary = Color(0xFF8B5CF6)
+val TeristaBackground = Color(0xFF0F172A)
+val TeristaSurface = Color(0xFF1E293B)
